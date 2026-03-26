49e951da02069834784d297af3d3f172b969bfa8
mariam antrobus <ma.cloudsandbox@gmail.com>
2026-03-26 11:54:48 +0000

    inital commit of product.go

---
+++ catalog/internal/application/core/domain/product.go
@@ -0,0 +1,29 @@
+package domain
+
+// TODO imports
+
+// Avoiding modelling attributes in initial stage
+// TODO update attribute types to domain types 
+type Product struct {
+     SKU 	  string /*sku*/		        `json: "sku"`
+     Name 	  string /*productName*/           `json: "name"`
+     Brand 	  string /*manufacturer*/	        `json: "brand"`
+     UnitPrice 	  float64 /*price*/		        `json: "unit_price"`
+     Sizes 	  []string /*size*/		        `json: "sizes"`
+     Description  string /*productDescription*/    `json: "description"`
+     Category 	  string /*category*/              `json: "category"`
+     Subcategory  string /*subCategory*/	        `json: "sub_category"` 
+}
+
+/*
+type productName string
+type manufacturer string
+type price float64 // TODO - create custom type
+type size []string // TODO - create custom type
+type productDescription string
+type sku string
+type category string // TODO - CREATE ENUM
+type subCategory string // TODO - CREATE ENUM
+type rating float32
+type review string // TODO - CREATE STRUCT{}
+*/
