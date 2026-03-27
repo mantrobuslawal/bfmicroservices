97adb7868403ae1ae2c5de197716008ed69a8688
mariam antrobus <ma.cloudsandbox@gmail.com>
2026-03-27 13:04:26 +0000

    Created port package files  api.go and db.go. Added get method for catalog db interface.

---
+++ catalog/internal/ports/db.go
@@ -0,0 +1,12 @@
+package ports
+
+import (
+   "context"
+   "github.com/mantrobuslawal/bfmircroservices/catalog/internal/application/core/domain"
+)
+
+type DBPort interface {
+     Get(ctx context.Context, searchType domain.SearchType) ([]domain.Product, error)
+}
+
+
