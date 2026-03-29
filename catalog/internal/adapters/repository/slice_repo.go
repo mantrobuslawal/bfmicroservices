d4abecda08beff0c2c82a2a5b1c87a0b1426b5b9
mariam antrobus <ma.cloudsandbox@gmail.com>
2026-03-29 19:27:11 +0100

    fixed errors in slice_repo.go

--- catalog/internal/adapters/repository/slice_repo.go
+++ catalog/internal/adapters/repository/slice_repo.go
@@ -14,47 +14,49 @@ const STD_QUERY_SIZE = 1
 type sliceRepo []domain.Product
 
 type Adapter struct {
-     repo *sliceRepo
+     repo sliceRepo
 }
 
 // GetProducts searches catalog repo, filtering records based on property chosen by client
 func (a Adapter) GetProducts(ctx context.Context, query domain.SearchType) ([]domain.Product, error) {	
-	results := nil	// Return empty slice if no match found
+	var results []domain.Product	// Return empty slice if no match found
 	field := query.Opt.String()
 	searchValue1 := query.SearchValue[0]
+	var searchValue2 string
+	if len(query.SearchValue) > STD_QUERY_SIZE {
+		searchValue2 = query.SearchValue[1]
+	} else {
+		searchValue2 = ""
+	}
 	
 // Repitition exists in algorithm due to ability to search on any struct property and
 // because I'm using a slice as temp db - will change implementation to real db 
 	switch field {
 		case "sku":
-                for product := range *a.repo {
-			if product.SKU == searchValue {
-				if results == nil { make([]domain.Product) }
+                for _, product := range a.repo {
+			if product.SKU == searchValue1 {
 				results = append(results, product)
 			}
 		}
 		
 		case "name":
-                for product := range *a.repo {
-			if product.Name == searchValue {
-				if results == nil { make([]domain.Product) }
+                for _, product := range a.repo {
+			if product.Name == searchValue1 {
 				results = append(results, product)
 			}
 		}
 		case "brand":
-                for product := range *a.repo {
-			if product.Brand == searchValue {
-				if results == nil { make([]domain.Product) }
+                for _, product := range a.repo {
+			if product.Brand == searchValue1 {
 				results = append(results, product)
 			}
 		}
 		case "category":
-                for product := range *a.repo {
-			if product.Category == searchValue {
+                for _, product := range a.repo {
+			if product.Category == searchValue1 {
 		// The second case represents when there is no subcategory to filter.
 		// It is always true, tested in toplevel if product.Category == searchValue
 				if (product.Subcategory != "" && product.Subcategory == searchValue2) || true  { 
-					if results == nil { make([]domain.Product) }
 				        results = append(results, product)
 				}
 			}
@@ -68,8 +70,8 @@ func (a Adapter) GetProducts(ctx context.Context, query domain.SearchType) ([]do
 	return results, nil
 }
 
-func NewAdapater(repo []domain.Product) (*Adapter, error) {
-	return &Adapter{repo: repo}, nil
+func NewAdapater(repo []domain.Product) (Adapter, error) {
+	return Adapter{repo: repo}, nil
 }
 
 
